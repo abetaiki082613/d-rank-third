2e3a75b71609b72750a238ca9d33de07bf917c5e
abe <abetaiki0526@yahoo.ne.jp>
2022-04-27 12:18:05 +0900

    revised

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(function () {
     $("#area_gNav-menu_open").removeClass("open");
     $("#area_btn_gNav").removeClass("open");
   });
-  $("#area_gNav_click_btn_close span").on("click", function () {
+  $("#area_gNav_click_btn_close i").on("click", function () {
     $("#area_gNav-menu_open").removeClass("open");
     $("#area_btn_gNav").removeClass("open");
   });
